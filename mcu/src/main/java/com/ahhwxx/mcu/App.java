e1f6f92f9b3b26e0fde8bc72a13cf18958914a18
夏小歌 <xiaweifeng@live.cn>
2020-12-23 15:10:36 +0800

    Hades：上传到jcenter使用插件的第一种方式

---
+++ mcu/src/main/java/com/ahhwxx/mcu/App.java
@@ -0,0 +1,12 @@
+package com.ahhwxx.mcu;
+
+import android.app.Application;
+
+/**
+ * @ClassName: App
+ * User: xiaxiaoge
+ * Date: 2020/12/21
+ * @Description:
+ */
+public class App extends Application {
+}
